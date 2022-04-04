a9faa2e7e4037a20327ca4550c4a55ed13facd57
Quincy Sesay <98617165+quincysesay@users.noreply.github.com>
2022-04-04 10:23:44 +0100

    Add files via upload

---
+++ Bookmark_manager/app.rb
@@ -0,0 +1,46 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+require './lib/bookmark'
+
+class BookmarkManager < Sinatra::Base
+  enable :sessions, :method_override
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Bookmark Manager'
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
+  run! if app_file == $0
+end
